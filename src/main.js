610aec10cd9e86adabeae6fdf7fdb58b5d9d333a
xiaolin <462441355@qq.com>
2018-04-03 21:54:26 +0800

    * 删除pages/log

--- src/main.js
+++ src/main.js
@@ -13,7 +13,6 @@ export default {
     // 页面前带有 ^ 符号的，会被编译成首页，其他页面可以选填，我们会自动把 webpack entry 里面的入口页面加进去
     pages: [
       '^pages/index/main',
-      'pages/logs/main',
       'pages/time/main'
     ],
     window: {
