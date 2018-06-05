89cb6bfe36cd95d85848e440b1d5b0b8bfb7f3eb
xiaolin <462441355@qq.com>
2018-06-05 10:37:01 +0800

    * 弹层组件

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,22 +1,43 @@
 <template>
   <div class="container">
-    Index
+    <button @click="showHandler">点我打开</button>
+    <comModal @closeModal="closeModal" :isShow="is_show">
+      <span slot="title">点击x关闭</span>
+      <div slot="content">
+        <div  v-for="(item,index) in 2" :key="index">
+          我是内容
+        </div>
+      </div>
+    </comModal>
     <rich-text :nodes="content"></rich-text>
   </div>
 </template>
 
 <script>
+import comModal from '@/components/model'
+
 export default {
   data () {
     return {
-      content: `<div style="color:red">dsads</div>`
+      content: `<div style="color:red">测试html</div>`,
+      is_show: false
     }
   },
   mounted () {
     console.log('Index')
   },
 
+  components: {
+    comModal
+  },
+
   methods: {
+    closeModal () {
+      this.is_show = false
+    },
+    showHandler () {
+      this.is_show = true
+    }
   }
 }
 </script>
